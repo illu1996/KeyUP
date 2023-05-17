e3b3f67a70218b9ed9b7259a0a282e56a78e8cb8
illu1996 <yamcys@nate.com>
2023-05-18 08:48:07 +0900

    무비 모델 수정 및 장르 모델 생성

--- final-pjt-back/movie/models.py
+++ final-pjt-back/movie/models.py
@@ -2,18 +2,20 @@ from django.db import models
 from django.conf import settings
 
 # Create your models here.
+class Genre(models.Model):
+    name: models.TextField()
+    
 class Movie(models.Model):
     adult = models.BooleanField()
     title = models.TextField()
     original_title = models.TextField()
-    genre_id = models.TextField()
     overview = models.TextField()
     poster_path = models.TextField()
-    release_data = models.DateField()
+    release_date = models.DateField()
     vote_average = models.FloatField()
     vote_count = models.IntegerField()
     like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_movies')
-    
+    genre_ids = models.ManyToManyField(Genre, related_name='movie_genre')
 
 class Review(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
