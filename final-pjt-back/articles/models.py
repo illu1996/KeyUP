964991850e01230c8e7a6003de49e80214c74aba
illu1996 <illu140411@gmail.com>
2023-05-17 19:51:41 +0900

    update article model

--- final-pjt-back/articles/models.py
+++ final-pjt-back/articles/models.py
@@ -13,6 +13,6 @@ class Article(models.Model):
 class Comment(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
-    content = models.CharField()
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
