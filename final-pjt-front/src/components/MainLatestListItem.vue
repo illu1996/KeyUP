7b59ea7ff0e3f50755460d1c332a404930b6b114
Yeoni <siunaus224@gmail.com>
2023-05-18 22:33:55 +0900

    메인화면 인기순위

--- final-pjt-front/src/components/MainLatestListItem.vue
+++ final-pjt-front/src/components/MainLatestListItem.vue
@@ -1,14 +1,14 @@
 <template>
-
-    <div href="javascript:void(0)" class="overlay-img">
-      <img :src="getImage" alt="NONONO" style="width:100px; height:100px;">
-      <div class="overlay"></div>
-      <div class="des">
-        <h1 class="title" id="card1">{{ movie.title }} {{ movie.vote_average }}</h1>
-        <h6 class="subtitle" id="card1-text"> {{ movie.overview }}</h6>
+  <div>
+    <div class="card" style="width: 18rem;" @mouseover="showDetails = true" @mouseout="showDetails = false">
+      <img :src="getImage" class="card-img-top" alt="NONONO">
+      <div class="card-overlay" :class="{ 'show-details': showDetails }">
+        <h5 class="card-title">{{ movie.title }}</h5>
+        <h6>{{ movie.vote_average }}</h6>
+        <p class="card-text">{{ truncateOverview }}</p>
       </div>
     </div>
-
+  </div>
 </template>
 
 <script>
@@ -17,13 +17,57 @@ export default {
   props: {
     movie: Object,
   },
+  data() {
+    return {
+      showDetails: false
+    };
+  },
   computed: {
     getImage() {
       return `https://image.tmdb.org/t/p/original/${this.movie.poster_path}`
+    },
+    truncateOverview() {
+      if (this.movie.overview.length > 30) {
+        return this.movie.overview.slice(0, 30) + '...';
+      } else {
+        return this.movie.overview;
+      }
     }
   }
-
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.card {
+  position: relative;
+  display: inline-block;
+}
+
+.card-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.8);
+  opacity: 0;
+  transition: opacity 0.3s ease-in-out;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.show-details {
+  opacity: 1;
+}
+
+.card-overlay h5,
+.card-overlay h6,
+.card-overlay p {
+  margin: 0;
+  padding: 0;
+  color: white;
+}
+</style>
